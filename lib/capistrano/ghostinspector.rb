dcfed42a6676b5fc55581b0e6b03b83b05ca5d83
Steven Richardson <srichardson@sessiondigital.com>
2015-10-18 12:27:10 +0100

    Move configuration to seperate task

--- lib/capistrano/ghostinspector.rb
+++ lib/capistrano/ghostinspector.rb
@@ -1,3 +1,4 @@
+require "capistrano/ghostinspector/config"
 require "capistrano/ghostinspector/version"
 require "capistrano/ghostinspector/arrays"
 require "capistrano/ghostinspector/api"
@@ -7,54 +8,59 @@ module Capistrano
   module Ghostinspector
     def self.load_into(config)
       config.load do
-        after "deploy", "capistrano:ghostinspector:run"
+        after "deploy", "capistrano:ghostinspector:configure"
+        after "capistrano:ghostinspector:configure", "capistrano:ghostinspector:run"
 
         namespace :capistrano do
           namespace :ghostinspector do
             task :run, :only => { :primary => true } do
 
-              set :giconfig = YAML::load(File.read("gi_config.yaml"))
+              # Capistrano::Ghostinspector.configure(config)
 
-              # Ghost Inspector API key
-              set :gi_api_key, giconfig["APIKEY"]
+              # set :giconfig = YAML::load(File.read("gi_config.yaml"))
 
-              # Google Analytics Tracking Property
-              set :ga_property, giconfig["ga_property"]
+              # # Ghost Inspector API key
+              # set :gi_api_key, giconfig["APIKEY"]
 
-              # Get tests and suites from command line
-              set :gitest, fetch(:gitest, nil)
-              set :gisuite, fetch(:gisuite, nil)
+              # # Google Analytics Tracking Property
+              # set :ga_property, giconfig["ga_property"]
 
-              # Check if GI is enabled for this deployment (Default: true)
-              set :gi_enabled, fetch(:gi_enabled, giconfig["gi_enabled"])
+              # # Get tests and suites from command line
+              # set :gitest, fetch(:gitest, nil)
+              # set :gisuite, fetch(:gisuite, nil)
 
-              # Should we rollback on failed GI tests (Default: true)
-              set :rollback, fetch(:rollback, giconfig["rollback"])
+              # # Check if GI is enabled for this deployment (Default: true)
+              # set :gi_enabled, fetch(:gi_enabled, giconfig["gi_enabled"])
 
-              if (gi_enabled == true)
+              # # Should we rollback on failed GI tests (Default: true)
+              # set :rollback, fetch(:rollback, giconfig["rollback"])
 
-                # run each test
-                Capistrano::Ghostinspector.getTests(gitest, giconfig["tests"]).each do |test|
-                  puts "* * * Running Ghost Inspector Test * * *"
-                  set :passing, Capistrano::Ghostinspector.executeApi(config, "tests", test)
-                end
+              puts(rollback)
 
-                # run each suite
-                Capistrano::Ghostinspector.getTests(gisuite, giconfig["suites"]).each do |suite|
-                  puts "* * * Running Ghost Inspector Suite * * *"
-                  set :passing, Capistrano::Ghostinspector.executeApi(config, "suites", suite)
-                end
+              # if (gi_enabled == true)
 
-                # If any test fails and the stage allows rollbacks then
-                # rollback to previous version.
-                if (passing == false && rollback == true)
-                  puts "* * * Ghost Inspector Failed. Rolling back * * *"
-                  run_locally %{cap #{stage} deploy:rollback}
-                else
-                  puts "* * * Ghost Inspector Complete. Deployment Complete * * *"
-                end
+              #   # run each test
+              #   Capistrano::Ghostinspector.getTests(gitest, giconfig["tests"]).each do |test|
+              #     puts "* * * Running Ghost Inspector Test * * *"
+              #     set :passing, Capistrano::Ghostinspector.executeApi(config, "tests", test)
+              #   end
 
-              end
+              #   # run each suite
+              #   Capistrano::Ghostinspector.getTests(gisuite, giconfig["suites"]).each do |suite|
+              #     puts "* * * Running Ghost Inspector Suite * * *"
+              #     set :passing, Capistrano::Ghostinspector.executeApi(config, "suites", suite)
+              #   end
+
+              #   # If any test fails and the stage allows rollbacks then
+              #   # rollback to previous version.
+              #   if (passing == false && rollback == true)
+              #     puts "* * * Ghost Inspector Failed. Rolling back * * *"
+              #     run_locally %{cap #{stage} deploy:rollback}
+              #   else
+              #     puts "* * * Ghost Inspector Complete. Deployment Complete * * *"
+              #   end
+
+              # end
             end
           end
         end
---
+++ lib/capistrano/ghostinspector/config.rb
@@ -0,0 +1,36 @@
+require "capistrano/ghostinspector/analytics"
+
+module Capistrano
+  module Ghostinspector
+    def self.configure(config)
+
+    	namespace :capistrano do
+          namespace :ghostinspector do
+            task :configure, :only => { :primary => true } do
+
+				set :giconfig = YAML::load(File.read("gi_config.yaml"))
+
+				# Ghost Inspector API key
+				set :gi_api_key, giconfig["APIKEY"]
+
+				# Google Analytics Tracking Property
+				set :ga_property, giconfig["ga_property"]
+
+				# Get tests and suites from command line
+				set :gitest, fetch(:gitest, nil)
+				set :gisuite, fetch(:gisuite, nil)
+
+				# Check if GI is enabled for this deployment (Default: true)
+				set :gi_enabled, fetch(:gi_enabled, giconfig["gi_enabled"])
+
+				# Should we rollback on failed GI tests (Default: true)
+				set :rollback, fetch(:rollback, giconfig["rollback"])
+
+				end
+			end
+		end
+
+    end
+
+  end
+end
